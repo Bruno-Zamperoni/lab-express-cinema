d9d1017b153ae8abf4447a17042f97e0eb57a1ab
Bruno-Zamperoni <bzamperoni@gmai.com>
2024-05-27 20:35:19 -0300

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movies = require("../models/movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movies.find().then((data) => {
+    res.render("movies", { movies: data });
+  });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movies.findById(req.params.id).then((data) => {
+    res.render("movie-details", { movie: data });
+  });
+});
 
 module.exports = router;
